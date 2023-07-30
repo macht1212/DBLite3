27f6c6c7d9efb38cf2be18a71480bff5064821c9
Alexander Nazimov <nasimov.alexander@gmail.com>
2023-07-30 17:45:12 +0400

    added four features to select

--- DBLite3/_select.py
+++ DBLite3/_select.py
@@ -138,11 +138,108 @@ def select_all_values_in_collection(db_name: str, collection: str) -> dict:
         raise ValueError(f'Error retrieving data from {collection} in {db_name}: {e}')
 
 
-# TODO
-def gt(db_name: str, collection: str, obj_name: str, target: int | float) -> list:
+def select_value_by_id_with_index(db_name: str, collection: str, obj_name: str, id: int) -> Any:
     """
     Objective:
-    The objective of the 'gt' function is to retrieve a list of values and their indices from a specific collection and object in a database dictionary, where the value is greater than a given target value. The function also checks if the values are numeric and raises a custom SelectError if they are not.
+    The objective of the function is to select a value from a specific collection and object in a database dictionary based on the given ID. The function returns the value and its index in the form of a string.
+
+    Inputs:
+        - db_name: a string representing the name of the database to be searched
+        - collection: a string representing the name of the collection to be searched
+        - obj_name: a string representing the name of the object to be searched
+        - id: an integer representing the ID of the value to be selected
+
+    Flow:
+        - Check if the input parameters are of the correct type
+        - Open the database using the _open_db function
+        - Check if the object is empty or if the ID is greater than the length of the values list
+        - If the object is not empty and the ID is valid, select the value and its index from the values list
+        - Return the value and its index in the form of a string
+
+    Outputs:
+        - A string containing the selected value and its index
+
+    Additional aspects:
+        - The function raises custom exceptions if there are errors during the selection process
+        - The function assumes that the values list exists in the object in the collection
+        - The function assumes that the ID is a valid index in the values list
+    """
+    if not isinstance(db_name, str):
+        raise KeyError('DB name must be a string.')
+    if not isinstance(collection, str):
+        raise KeyError('Collection name must be a string.')
+    if not isinstance(obj_name, str):
+        raise KeyError('Object name must be a string.')
+    if not isinstance(id, int):
+        raise KeyError('Target must be numeric.')
+
+    try:
+        DATABASE = _open_db(db_name=db_name)
+    except OpenError as e:
+        raise OpenError(f'Error: {e}')
+
+    if not _is_value_in(DB=DATABASE, collection=collection, obj_name=obj_name):
+        raise SelectError("Object is empty.")
+    elif id > len(DATABASE[collection][obj_name]['values'])+1:
+        raise SelectError("Length of values less then id number.")
+    else:
+        answer = DATABASE[collection][obj_name]['values'][id-1]
+        return f'Value: {answer[1]}, Index: {answer[0]}'
+
+
+def select_value_by_id_without_index(db_name: str, collection: str, obj_name: str, id: int) -> Any:
+    """
+    Objective:
+    The objective of the select_value_by_id_without_index function is to select a value from a specific collection and object in a database dictionary based on the given ID. The function returns the value and its index in the form of a string. The function raises custom exceptions if there are errors during the selection process.
+
+    Inputs:
+        - db_name: a string representing the name of the database to be searched
+        - collection: a string representing the name of the collection to be searched
+        - obj_name: a string representing the name of the object to be searched
+        - id: an integer representing the ID of the value to be selected
+
+    Flow:
+        - The function checks if the input parameters are of the correct type.
+        - The function opens the database using the _open_db function.
+        - The function checks if the object is empty or if the ID is greater than the length of the values list.
+        - If the object is not empty and the ID is valid, the function selects the value and its index from the values list.
+        - The function returns the value and its index in the form of a string.
+
+    Outputs:
+        - A string containing the selected value and its index.
+
+    Additional aspects:
+        - The function assumes that the values list exists in the object in the collection.
+        - The function assumes that the ID is a valid index in the values list.
+    """
+    if not isinstance(db_name, str):
+        raise KeyError('DB name must be a string.')
+    if not isinstance(collection, str):
+        raise KeyError('Collection name must be a string.')
+    if not isinstance(obj_name, str):
+        raise KeyError('Object name must be a string.')
+    if not isinstance(id, int):
+        raise KeyError('Target must be numeric.')
+
+    try:
+        DATABASE = _open_db(db_name=db_name)
+    except OpenError as e:
+        raise OpenError(f'Error: {e}')
+
+    if not _is_value_in(DB=DATABASE, collection=collection, obj_name=obj_name):
+        raise SelectError("Object is empty.")
+    elif id > len(DATABASE[collection][obj_name]['values']) + 1:
+        raise SelectError("Length of values less then id number.")
+    else:
+        answer = DATABASE[collection][obj_name]['values'][id - 1]
+        return f'Value: {answer[1]}'
+
+
+
+def gt(db_name: str, collection: str, obj_name: str, target: numbers.Number) -> list:
+    """
+    Objective:
+    The 'gt' function aims to retrieve a list of values and their indices from a specific collection and object in a database dictionary, where the value is greater than a given target value. The function also checks if the retrieved values are numeric and raises a custom SelectError if they are not.
 
     Inputs:
         - db_name: a string representing the name of the database to retrieve the values from.
@@ -150,8 +247,9 @@ def gt(db_name: str, collection: str, obj_name: str, target: int | float) -> lis
         - obj_name: a string representing the name of the object to retrieve the values from.
         - target: an integer or float representing the target value to compare the retrieved values to.
 
-        Flow:
-        - The function first tries to open the database using the '_open_db' function and checks if the first value in the collection is numeric using the '_is_value_in' function.
+    Flow:
+        - The function first checks if the input parameters are valid.
+        - The function then tries to open the database using the '_open_db' function and checks if the first value in the collection is numeric using the '_is_value_in' function.
         - If the first value is not numeric, the function raises a SelectError.
         - The function then retrieves a list of values and their indices from the specified collection and object in the database dictionary where the value is greater than the target value.
         - The retrieved values and indices are returned as a list of lists.
@@ -164,10 +262,20 @@ def gt(db_name: str, collection: str, obj_name: str, target: int | float) -> lis
         - The function raises a SelectError if the retrieved values are not numeric.
         - The function assumes that the specified collection and object exist in the database dictionary.
     """
+
+    if not isinstance(db_name, str):
+        raise KeyError('DB name must be a string.')
+    if not isinstance(collection, str):
+        raise KeyError('Collection name must be a string.')
+    if not isinstance(obj_name, str):
+        raise KeyError('Object name must be a string.')
+    if not isinstance(target, numbers.Number):
+        raise KeyError('Target must be numeric.')
+
     try:
         DATABASE = _open_db(db_name=db_name)
 
-        if _is_value_in(DATABASE, collection, obj_name):
+        if _is_value_in(DB=DATABASE, collection=collection, obj_name=obj_name):
             for value in DATABASE[collection][obj_name]['values']:
                 if not isinstance(value[1], numbers.Number):
                     raise SelectError('This method is allowed only for numeric values.')
@@ -179,6 +287,48 @@ def gt(db_name: str, collection: str, obj_name: str, target: int | float) -> lis
 
 
 
-# TODO
-def lt(db_name: str, collection: str, obj_name: str, target: int | float) -> list:
-    pass
+def lt(db_name: str, collection: str, obj_name: str, target: numbers.Number) -> list:
+    """
+    Objective:
+    The objective of the 'lt' function is to return a list of values and their indices from a specific collection and object in a database that are less than a given target value. The function also performs input validation and checks if the values in the collection are numeric.
+
+    Inputs:
+        - db_name: a string representing the name of the database to be queried.
+        - collection: a string representing the name of the collection to be queried.
+        - obj_name: a string representing the name of the object to be queried.
+        - target: a numeric value representing the target value to compare the values in the collection against.
+
+    Flow:
+        - The function performs input validation to ensure that the inputs are of the correct type.
+        - The function opens the database using the '_open_db' function and checks if the first value in the collection is numeric using the '_is_value_in' function.
+        - The function returns a list of values and their indices from the collection and object that are less than the target value.
+
+    Outputs:
+        - A list of values and their indices from the collection and object that are less than the target value.
+
+    Additional aspects:
+        - The function raises a 'KeyError' if any of the inputs are of the incorrect type.
+        - The function raises a 'SelectError' if the values in the collection are not numeric.
+        - The function uses the '_open_db' and '_is_value_in' functions to access the database and check the values in the collection.
+        - The function assumes that the 'values' key exists in the object in the collection.
+    """
+    if not isinstance(db_name, str):
+        raise KeyError('DB name must be a string.')
+    if not isinstance(collection, str):
+        raise KeyError('Collection name must be a string.')
+    if not isinstance(obj_name, str):
+        raise KeyError('Object name must be a string.')
+    if not isinstance(target, numbers.Number):
+        raise KeyError('Target must be numeric.')
+
+    try:
+        DATABASE = _open_db(db_name=db_name)
+
+        if _is_value_in(DB=DATABASE, collection=collection, obj_name=obj_name):
+            for value in DATABASE[collection][obj_name]['values']:
+                if not isinstance(value[1], numbers.Number):
+                    raise SelectError('This method is allowed only for numeric values.')
+    except OpenError as e:
+        raise OpenError(f'Error: {e}')
+
+    return [[f'Value: {v[1]}, index: {v[0]}'] for v in DATABASE[collection][obj_name]['values'] if v[1] < target]
